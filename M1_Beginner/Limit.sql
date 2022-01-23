5084c0a64427f21c2cd126fd6c89326206ddc00b
pradeepradee1 <pradeepradee1@gmail.com>
2022-01-23 16:22:45 +0530

    added upto group by

---
+++ M1_Beginner/Limit.sql
@@ -0,0 +1,35 @@
+
+
+SELECT 
+* 
+from CAR c
+order by car_id ASC 
+LIMIT 2
+
+SELECT 
+* 
+from CAR c
+order by car_id ASC 
+LIMIT 2,3
+
+
+SELECT 
+* 
+from CAR c
+order by car_id ASC 
+LIMIT 0,2
+
+SELECT 
+* 
+from CAR c
+order by car_id ASC 
+LIMIT 2
+
+
+SELECT 
+* 
+from CAR c
+order by car_id ASC 
+LIMIT 2 OFFSET 3
+
+
