fcfe17e37353add9f4e2d6aecfd79c903ef6528e
pradeepradee1 <pradeepradee1@gmail.com>
2022-04-19 20:00:02 +0530

    Added Defintions and OrderOfExecutions

--- M1_Beginner/L1_Intro/L0_Definitions.sql
+++ M1_Beginner/L1_Intro/L0_Definitions.sql
@@ -1,24 +1,29 @@
 /*
 
-Data	:		
-			Collection of information of any one Business entity is known as data.
-
+Data		:
+			
+				Collection of information of any one Business entity is known as data.
+			
 Database	:
 			
-			It is software, which stores and manages the collection of information of all objects in the business.	
+				It is software, 
+				which stores the collection of information of all objects
+			
 */
 
 #Table Structure :
 
 #Table	= 	Entity
-#Row		=	Tuple
+#Row	=	Tuple
 #Column	=	Attribute
 #Cell	=	Field
 
 /*
 Database Management Systems(DBMS):
 		
-		A Collection of programs which enables user to access database , manipulate data and represent data
+		A Collection of programs 
+		which enables user to access database and represent data
+		For ex : Manager access the employee and represent work to client
 
 
 Types DBMS :
@@ -27,26 +32,31 @@ Types DBMS :
 
 			2) Network DBMS
 
-			3) Relatandional DBMS
+			3) Relational DBMS
 
 			4) Object-oriented DBMS
 
-			
+
 1) Hierarchical DBMS :
-						parent-child relationship of storing data.
-						It has a structure like a tree with node representing records and branches representing fields
+						This Model follows the parent-child relationship for storing data.
+
 
 2) Network DBMS	:
-						Supports many-to many relations.
-						This usally results in complex database structures
+						This Model follows the many-to many relations.
+		
 
 3) Relational DBMS :
-						defines database relationships in form of tables , also know as relations
-						For ex : 1) MS SQL 2) MYSQL
+						It is collections of information in the form of tables with relations
+						For ex : 
+								 1) MS SQL 
+								 2) MYSQL
 
 4) Object-Oriented DBMS :
-						the data to be stored is in form of objects
-						For ex: postgres SQL			
+						
+						It is similar to RDBMS
+						but It Stores data  in the form of objects.
+						For ex: 
+								 1) postgres SQL			
 
 */
 
@@ -57,12 +67,12 @@ Types DBMS :
 /*
 1) OLTP RDBMS	: Online Transactions Processing
 				
-				It contains Transactinal data/ day to day data / Current data / dynamic data
+				It contains Transactinal data (or) day to day data (or) dynamic data
 				It is used to store or process the business data
 
-2) OLAP RDMS   	: On Line Analtical Processing
+2) OLAP RDBMS   : OnLine Analtical Processing
 
-				It contains historical data / old data.
+				It contains historical data.
 				It is used to analyze the business.
 */
 
@@ -72,23 +82,26 @@ Types DBMS :
 DataModel :
 
 		For any OLTP RDBMS the data model is E-R ( Entity-Relationship ) Model.
-		Create an Entity-Relationship (ER) model is to visually represent the structure of a business database.
+		ER model is to visually represent the structure of a business database.
+
 
 Entity And Its Attributes :
 
-		Composite			: 	Can be divided into sub parts (name - > firstname,secondname,lastname)
+		Composite 			: 	Can be divided into sub parts (name - > firstname,secondname,lastname)
 		
-		Simply or atomic	: 	Which are not divisiable are simply know as simple or atomic attributes 
+		Simply or Atomic	: 	Which are not divisiable are simply know as simple or atomic attributes 
 																					(housenumber,phone)
 
-		Single		:	single value for a particular entity know as single-valued attributes
+		Single				:	single value for a particular entity know as single-valued attributes
 		
-		Multi-Valued :	most Number of values allowed for each individual entity
+		Multi-Valued 		:	most Number of values allowed for each individual entity
 
 
-		Stored 		:	Stored attribute are such attribute which are already stored in the database and forms which 				 the value of another attribute is derived is called stored attribute. (Birth_date)
+		Stored 				:	Attributes which are already stored in the database(Birth_date)
+		 				
 
-		Derived		:	Attributes which are derived from the real entities are known as derived attributes (Age)
+		Derived				:	Attributes which are derived from the real entities are known as derived attributes 
+																											(Age)
 
 
 
@@ -99,8 +112,8 @@ Entity And Its Attributes :
 /*
  
 MYSQL :
-		A Relational Database managment Systems
 		An Open-Source Software 
+		A Relational Database managment Systems
 		Provides multi-user access
 		Supports-multi-storage engines
 		Works on many platforms
