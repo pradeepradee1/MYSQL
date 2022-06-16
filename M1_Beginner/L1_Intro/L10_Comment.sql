a60bbe5e67c077d0fc8f612cf78aa6b701e28dee
pradeepradee1 <pradeepradee1@gmail.com>
2022-06-16 12:03:25 +0530

    Changes updated in comments

--- M1_Beginner/L1_Intro/L10_Comment.sql
+++ M1_Beginner/L1_Intro/L10_Comment.sql
@@ -8,25 +8,37 @@ multiple-line comment
 */
 
 #EXAMPLE:
+use mydb;
 SELECT * FROM car; -- this is comment
 
+#Using Comment Keyword
 CREATE or REPLACE TABLE stack1(
 id int COMMENT 'You can include genus, but never subspecies.',
 name varchar(10)
 );
 
 
-CREATE TABLE bird (
+CREATE or REPLACE TABLE bird (
 bird_id INT NOT NULL AUTO_INCREMENT,
 species VARCHAR(300) DEFAULT NULL COMMENT 'You can include genus, but never subspecies.',
 INDEX idx_species (species) COMMENT 'We must search on species often.',
 PRIMARY KEY (bird_id)
 ) ENGINE=InnoDB COMMENT 'This table was inaugurated on February 10th.';
 
+#View Comments
+show full columns from bird;
+
+
+ALTER table bird modify column bird_id tinyint comment "Changes in bird_id data type";
+
+
+#View Comments
+show full columns from bird;
+
 
 #NOTE:
-#These comments, unlike the others, are saved with the schema and can be retrieved via SHOW CREATE TABLE or from information_schema .
+#1) These comments, unlike the others, are saved with the schema and can be retrieved via SHOW CREATE TABLE or from information_schema .
 
-#How Do i See The Comment 
+#Another Way
 show create table bird;
 
