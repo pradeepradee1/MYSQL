5cbb4778c3bf4113c9012f538f6bdb7ee5c2551d
pradeepradee1 <pradeepradee1@gmail.com>
2023-01-15 01:18:41 +0530

    Added Procedure and Dump

---
+++ M7_Expert/M1_Procedure/M1_Introduction.sql
@@ -0,0 +1,34 @@
+/*
+What Is Procedure ?
+
+		It is block of code and stored in the database
+
+		Here Block Of Code includes the 
+			1)	SQL Queries 
+			2)	DML,DCL,DDL and TCL Commands
+			3) 	Collection Types
+			4)	Cursors
+			5)	Loop and If Else Statements
+			6)	Variables
+			7)	Exception Handling
+			8)	Etc
+
+*/
+
+
+
+
+create or replace procedure pr_name (p_name varchar ,p_age int)
+as 
+declare 
+	variable data type;
+begin 
+	procedure body = all logics
+end
+
+
+
+declare 	as 	Optional
+begin 		as 	Mandatory
+EXCEPTION 	as 	Optional
+end 		as 	Mandatory
---
+++ M7_Expert/M1_Procedure/M2_Procedure.sql
@@ -0,0 +1,42 @@
+create procedure pr_buy_prducts()
+begin
+	declare v_product_name varchar(100);
+	declare v_price int;
+
+	select productname,price
+	into v_product_name,v_price
+	from Productv1
+	where productname="MacBookPro13";
+
+	insert into Salesv1 ( Store_id,product,cost )
+	values (11,v_product_name,v_price);
+	
+end
+
+
+
+
+call pr_buy_prducts(); 
+
+
+-- With Parameters
+
+create procedure pr_buy_prducts(p_prkduct_name varchar(100),p_price int)
+begin
+	declare v_product_name varchar(100);
+	declare v_price int;
+
+	select productname,price
+	into v_product_name,p_price
+	from Productv1
+	where productname="MacBookPro13";
+
+	insert into Salesv1 ( Store_id,product,cost )
+	values (11,v_product_name,v_price);
+	
+end
+
+
+
+
+call pr_buy_prducts1("MacBookPro13",100000);
