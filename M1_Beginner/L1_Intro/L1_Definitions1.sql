35a5edcd48ae62acffaf2a823094ba0b2966c11b
pradeepradee1 <pradeepradee1@gmail.com>
2023-02-11 17:01:37 +0530

    Added CAP in Definitions and Configurations

--- M1_Beginner/L1_Intro/L1_Definitions1.sql
+++ M1_Beginner/L1_Intro/L1_Definitions1.sql
@@ -7,21 +7,20 @@ Data				:
 Database			:
 			
 						Collection of information of all entity or objects or table will be stored in database
-			
+
 Types Of DataBase	:
 						1)	Transactional DataBase (RDBMS) 	Ex : Mysql,Postgresql,Oracle
 						2)	NOSQL DataBase				  	Ex : Hbase,Cassandra,MongoDB,CouchDB
-
+			
 
 1) 	Transactional DataBase	:
 								1)	Vertical Scalable (Scale Out)
-								2)	Stores data in row based and row-columner format
+								2)	Stores data in row based form
 								3)	Consistent (Multiple or concurrent user will get same answer)
 2)	NOSQL DataBase			:	
 								1)	Horizontal Scalable (Scale Up)
 								2) 	Stores data in Columner,key-value,Graph,document form
-								3)	Not Consistent (Multiple or concurrent user will not get same answer)
-										Here Consistency can be acheived but latency will be more
+								3)	Not Consistent as like RDBMS
 								4) 	Distributed
 								5)	Highly available If no Master-Slave Architecture
 
@@ -45,9 +44,11 @@ What is Transactional DataBase?
 
 Transactional DataBase Follows the ACID Properties :
 
-		A 	=	Atomicity 	(each operations will be atomic i.e it will be complete transaction)(single step)
-		
-		C  	=	Consistency	(After success or failure data should be consistent)
+		A 	=	Atomicity 	(entire transaction takes place at once , there is no midway , either success or failure)
+				if success	= commit will happen
+				if failure	= roll back will happen		
+				
+		C  	=	Consistency	(data should be consistent before and after the transaction)
 				
 				For Example	:	A is sending 100 amount to B 
 				success = if the transaction success 100 debited in A and 100 is credited in B (Consistent)
@@ -56,14 +57,21 @@ Transactional DataBase Follows the ACID Properties :
 				success	= commit will happen
 				failure	= roll back will happen
 				
-		I 	=	Isolation 	(each transaction will excute without impacting each other)
-
+		I 	=	Isolation 	(multiple transaction will excute without impacting each other)
+				
 				For Example :  A is writing the query , B is reading the query and C is Writing the query
 							   A,B,C are not impacting with each other
-
-		D   =   Durability 	(holding the data in the logs) 
-							(help us from failure and recovery)
 				
+		D   =   Durability 	(holding the data in the logs) 
+							(persist data from failure occurs)	
+
+
+Responsbility	:
+		
+Atomicity	=	Transaction Manager
+Consistency	=	Application programmer
+Isolation	=	Concurrency Control Manager
+Durability	=	Recovery Manager
 
 
 */
@@ -73,7 +81,7 @@ Transactional DataBase Follows the ACID Properties :
 
 CAP Theorem	:
 				There is no database as such which can provide all three properties at once
-				Only tow can be achived
+				Only two can be achived
 
 C	=	Consistency
 A 	=	Avaliablity (DB can easily and quickly UP , It will not take too much time)
@@ -85,9 +93,12 @@ P   = 	Partition tolerance (distibution)
 						  A-----P	
 					Cassandra,scyllaDB
 
-Consistency and availbility =	MYSQL
-Consistency and Partition	=	Hbase,MongoDB
-Availbility and Partition	=	Cassandra,ScyllaDB
+
+
+Consistency		=	Data is the same across the cluster , So you can read from any node and get same data
+Avaliablity		=	Ability to access the cluster , even if a node in the cluster goes down
+Partition		=	cluster continues to function even if there is a partition (communication break) between two node
+
 
 */
 
