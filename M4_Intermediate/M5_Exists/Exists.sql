141442f8fe8c6e091fccdbfa79f5bcd0ab5751e4
pradeepradee1 <pradeepradee1@gmail.com>
2023-03-05 21:24:19 +0530

    Added ProblemSolving

---
+++ M4_Intermediate/M5_Exists/Exists.sql
@@ -0,0 +1,39 @@
+/*
+
+Exists give bool function
+
+*/
+
+
+create table customers ( customer_id int, customer_name varchar(100), customer_email varchar(200), primary key (customer_id) );
+
+insert into customers values(1,'Shashank','abc@gmail.com');
+insert into customers values(2,'Rahul','aaa@gmail.com');
+insert into customers values(3,'Ajay','klm@gmail.com');
+insert into customers values(4,'Nitin','poc@gmail.com');
+insert into customers values(5,'Naveen','mnc@gmail.com');
+
+create table orders ( order_id int, customer_id int, amount float, status varchar(50), primary key (order_id) );
+
+insert into orders values(101,1,550,'Delivered');
+insert into orders values(102,2,350,'Delivered');
+insert into orders values(103,1,220,'Cancelled');
+insert into orders values(104,3,660,'Delivered');
+insert into orders values(105,3,300,'Delivered');
+
+
+select 
+c.*
+from 
+customers c 
+left join orders o on c.customer_id  = o.order_id 
+where o.customer_id is not null and c.customer_name = 'Shashank' ;
+
+
+
+select 
+c.*
+from 
+customers c 
+where EXISTS 
+(select order_id from orders o where c.customer_id  = o.order_id and c.customer_name = 'Shashank') ;
---
+++ M9_Inputs/M1_ProblemSolving/M2_GroupBy/problem6.sql
@@ -0,0 +1,61 @@
+/*
+
+Pivot Table :
+		
+		11	english	88
+		11	tamil	66
+		11	maths	85
+		12	english	81
+		12	tamil	71
+		12	maths	100
+
+Output Table :
+		
+		id 		 englishMarks 	tamilMarks	mathsMarks
+		11 			88				66			85
+		12 			81				71			100
+
+
+*/
+
+
+
+create table ps3
+(
+id tinyint,
+subject varchar(50),
+marks int
+)
+
+insert into ps3 
+values (11,"english",88),(11,"tamil",66),(11,"maths",85),(12,"english",81),(12,"tamil",71),(12,"maths",100)
+
+select * from ps3
+
+select 
+	*,
+	CASE when ps3.subject = "english" then ps3.marks else 0 END as english_details,
+	CASE when ps3.subject = "tamil" then ps3.marks else 0 END as tamil_details,
+	CASE when ps3.subject = "maths" then ps3.marks else 0 END as maths_details
+from 
+	ps3
+
+select 
+	*,
+	CASE when ps3.subject = "english" then ps3.marks else 0 END as english_details,
+	CASE when ps3.subject = "tamil" then ps3.marks else 0 END as tamil_details,
+	CASE when ps3.subject = "maths" then ps3.marks else 0 END as maths_details
+from 
+	ps3
+group by 
+	ps3.id 
+
+select 
+	*,
+	sum(CASE when ps3.subject = "english" then ps3.marks else 0 END) as english_details,
+	sum(CASE when ps3.subject = "tamil" then ps3.marks else 0 END) as tamil_details,
+	sum(CASE when ps3.subject = "maths" then ps3.marks else 0 END) as maths_details
+from 
+	ps3
+group by
+	ps3.id
