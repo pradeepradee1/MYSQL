d855d6e63e3d49b578629e9de1e205efef4cd5d5
pradeepradee1 <pradeepradee1@gmail.com>
2024-04-22 21:29:59 +0530

    IF_ELse

--- M1_Beginner/L4_FlowControl/Conditional_Statement/L2_Case.sql
+++ M1_Beginner/L4_FlowControl/Conditional_Statement/L2_Case.sql
@@ -7,6 +7,9 @@ CONDITIONAL_STATEMENTS
 #2) CASE 
 
 How it works ?	
+
+				It is similar to if_else in python
+
  				It will go first When , if it is true
 				Once a condition is true, it will stop reading (or) will'nt go the next when and return the result.
 */
