9f752e3e2a91d922c17e13ed74347375366dafed
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-06-25 15:33:14 +0530

    Update Q1.sql

--- M9_Inputs/M1_ProblemSolving/M4_Join/Q1.sql
+++ M9_Inputs/M1_ProblemSolving/M4_Join/Q1.sql
@@ -2,16 +2,19 @@ select * from problemsolving2_table1;
 select * from problemsolving2_table2;
 
 
-#Question 1 : 
 
+
+#Question 1 : 
+	
+# What is OP for inner join ?
+  #with null values also
+
+#Question 2 :
+	
 # What is OP for Left join ?
   #with null values also
 
-
-
-
-
-#Question 2 : 
+#Question 3 : 
 
 # What is OP for right join ?
-	#with null values also
+  #with null values also
