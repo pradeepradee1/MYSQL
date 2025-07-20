20926db591705513ca3031398e2333ed4cf17ea3
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-07-20 09:59:43 +0530

    Update M2_1NF.sql

--- M5_Senior/M1_NormalizationAndDeNormalization/M1_Normalization/M2_1NF.sql
+++ M5_Senior/M1_NormalizationAndDeNormalization/M1_Normalization/M2_1NF.sql
@@ -13,6 +13,7 @@ SELECT * from First_1NF_2;
 	2) Each Attribute should have unique name (phone_no shouldn't repeat)
 	3) Every Attribute should follow the same domain (Same Data type)
 	4) No Duplicate Rows
+	5) Primary key should be in this table for identify the records
 
 
 */
