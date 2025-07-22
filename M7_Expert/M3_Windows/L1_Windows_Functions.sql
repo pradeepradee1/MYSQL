6e099c4d02f6872dadbba9f159b1e6bceae8fbba
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-07-22 13:06:06 +0530

    Update L1_Windows_Functions.sql

--- M7_Expert/M3_Windows/L1_Windows_Functions.sql
+++ M7_Expert/M3_Windows/L1_Windows_Functions.sql
@@ -19,7 +19,7 @@ Dense Rank
 	*) If two rows are tied at rank 2, the next rank will be 3
 
 Row Number
-	*) It will generate the unique interger to identify the each records of the table
+	*) The unique interger to identify the each records of the table
 	*) No Gaps in numbering
 
 */
