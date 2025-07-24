abb0ef701ca7a8bcc29439c4bd2341cce3702e04
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-07-24 10:21:47 +0530

    Update M3_DB_NormAndDeNorm.sql

--- M5_Senior/M1_NormalizationAndDeNormalization/M3_DB_NormAndDeNorm.sql
+++ M5_Senior/M1_NormalizationAndDeNormalization/M3_DB_NormAndDeNorm.sql
@@ -21,8 +21,8 @@ Difference Between Normalization and DeNormalization
 	
 	Memory is not optimized in denormalized table because the same data is stored in different places
 
-6) 	It is optimized for the quick access and Data is frequently used for insert, update, and delete
+6) 	It is suit for OLTP and It is optimized for the quick access and Data is frequently used for insert, update, and delete
 	
-	It Optimized for the complex analytical queries on historical data and Data is frequently used for retrieval 
+	It is suit for OLAP and It Optimized for the complex analytical queries on historical data and Data is frequently used for retrieval 
 
 */
