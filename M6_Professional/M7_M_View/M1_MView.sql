f72d49491c559a6638d1f39f4e0b5a04c3297c71
Pradeep <pradeepradee1@gmail.com>
2025-07-28 12:32:17 +0530

    changes are added

--- M6_Professional/M7_M_View/M1_MView.sql
+++ M6_Professional/M7_M_View/M1_MView.sql
@@ -4,7 +4,7 @@ Materialized view :
 
                 IT is a database object
                 It is a physically table
-                It is stored result of a query and stored like a table
+                It is stored result of a query 
                 Unlike a regular view (It doesn't store data and fetches  data from live table), 
                 This improves performance, especially for complex aggregations or joins.
 
