7f9c0d23712b4eec7219667d1e39b9433de8594a
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-08-04 13:30:37 +0530

    Merge statement added

--- M5_Senior/M2_Merge/M1_Merge.sql
+++ M5_Senior/M2_Merge/M1_Merge.sql
@@ -3,6 +3,9 @@ Merge Statement : Not Supported in MYSQL
 
 It also known as "upsert" 
 It is used to perform insert, update, or delete operations in a single statement
+
+#Note
+Explain the merge statement
 					
 */
 
