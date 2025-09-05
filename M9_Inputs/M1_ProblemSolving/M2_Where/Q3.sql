3b0c18343216f477ad3f16667fcfe91454046c2c
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-09-05 10:08:09 +0530

    Create Q3.sql

---
+++ M9_Inputs/M1_ProblemSolving/M2_Where/Q3.sql
@@ -0,0 +1,35 @@
+/*
+
+Problem Statement
+
+From the orders table, find customers who placed at least one order in January but did not place any orders in February.
+
+*/
+
+CREATE TABLE orders (
+ order_id INT,
+ customer_id INT,
+ order_date DATE
+);
+
+INSERT INTO orders VALUES
+(1, 101, '2024-01-05'),
+(2, 102, '2024-01-10'),
+(3, 103, '2024-01-15'),
+(4, 101, '2024-02-03'),
+(5, 104, '2024-01-20'),
+(6, 105, '2024-01-25'),
+(7, 106, '2024-02-05'),
+(8, 102, '2024-02-10'),
+(9, 107, '2024-01-30'),
+(10, 108, '2024-03-01');
+
+
+SELECT DISTINCT customer_id
+FROM orders
+WHERE EXTRACT(MONTH FROM order_date) = 1
+ AND customer_id NOT IN (
+ SELECT customer_id
+ FROM orders
+ WHERE EXTRACT(MONTH FROM order_date) = 2
+ );
