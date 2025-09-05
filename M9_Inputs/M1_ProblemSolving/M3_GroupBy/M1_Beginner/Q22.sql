60a5f6c5ab9ee2953393503a538ee15b435f4b54
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-09-05 12:16:03 +0530

    Create Q22.sql

---
+++ M9_Inputs/M1_ProblemSolving/M3_GroupBy/M1_Beginner/Q22.sql
@@ -0,0 +1,25 @@
+/*
+
+From the purchase history, find users who bought at least one ‘Shirt’ AND one ‘Pant’.
+*/
+
+CREATE TABLE purchases (
+ user_id INT,
+ product_name VARCHAR(100)
+);
+
+INSERT INTO purchases VALUES
+(1, 'Shirt'),
+(1, 'Pant'),
+(1, 'Shoes'),
+(2, 'Shirt'),
+(3, 'Pant'),
+(4, 'Pant'),
+(4, 'Shirt'),
+(5, 'Shoes');
+
+SELECT user_id
+FROM purchases
+WHERE product_name IN ('Shirt', 'Pant')
+GROUP BY user_id
+HAVING COUNT(DISTINCT product_name) = 2;
