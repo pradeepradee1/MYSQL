ffb95933b84b8a1f8a7438d555b7782600148e7b
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2025-09-08 14:21:40 +0530

    Create Q13.sql

---
+++ M9_Inputs/M1_ProblemSolving/M6_Windows/Q13.sql
@@ -0,0 +1,30 @@
+/*
+
++----+-----+
+| id | num |
++----+-----+
+| 1 | 1 |
+| 2 | 1 |
+| 3 | 1 |
+| 4 | 2 |
+| 5 | 1 |
+| 6 | 2 |
+| 7 | 2 |
++----+-----+
+
+Expected Output:
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1 |
++-----------------+ 
+*/
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT id, num,
+           LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+           LEAD(num, 1) OVER (ORDER BY id) AS next_num
+    FROM Numbers
+) t
+WHERE num = prev_num AND num = next_num;
