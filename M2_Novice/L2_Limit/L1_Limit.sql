5dacd09e03fdbe3ba8a85e01b9ef520da65e0e3b
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-01-18 08:55:30 +0530

    Update L1_Limit.sql

--- M2_Novice/L2_Limit/L1_Limit.sql
+++ M2_Novice/L2_Limit/L1_Limit.sql
@@ -11,10 +11,12 @@ select * From Wheather w
 order by Temp 
 limit 2
 
-# Limit Offset
+# Limit using Offset
 select * From Wheather w 
 order by Temp
 limit 2,3
+
+/* LIMIT 2 OFFSET 3 */
 
 #Note:
 --      Skips the first 2 rows and returns the next 3.
@@ -22,7 +24,6 @@ limit 2,3
 select * From Wheather w 
 order by Temp
 limit 0,2
-
 
 /* LIMIT 2 OFFSET 2 */
 
