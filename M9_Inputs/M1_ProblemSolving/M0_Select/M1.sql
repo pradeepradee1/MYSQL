7d71dcd230780658453c6aa289145cb6ade6e8c1
Pradeep <pradeepradee1@gmail.com>
2026-01-30 14:23:27 +0530

    added the revenue and net profit

--- M9_Inputs/M1_ProblemSolving/M0_Select/M1.sql
+++ M9_Inputs/M1_ProblemSolving/M0_Select/M1.sql
@@ -18,7 +18,10 @@ Explanation : Click --> Views or Conversion (changes)
 Note : 
 DB Revenue and Profit ?
 
+Revenue :
 Revenue is the total income a business generates from sales or services, also known as the "top line". 
+
+Profit or Net Income : 
 Profit, or net income  is the money left over after all expenses (such as costs of goods, operating expenses, and taxes) 
 are subtracted from the revenue
 
