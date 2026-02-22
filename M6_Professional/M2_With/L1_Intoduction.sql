6f13b52126d4a3a2111ccbc4dd0707566ddbc4ad
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-02-22 08:35:53 +0530

    Update L1_Intoduction.sql

--- M6_Professional/M2_With/L1_Intoduction.sql
+++ M6_Professional/M2_With/L1_Intoduction.sql
@@ -6,10 +6,10 @@ What is With Clause or CTEs ?
 
 	*) It is Used to define a temporary result set (like a virtual table) using with clause 
 that can be referenced in the main query.
-	
 	*) Can define one or more temporary result sets
-	
 	*) Improves readability and modularity of complex SQL queries 
+	*) It support Recursive 
+
 
 
 Modularity =
