86e785f7bc518f6b38e92515790b8956720d12dd
Pradeep <pradeepradee1@gmail.com>
2026-02-23 10:33:27 +0530

    changes added

---
+++ M4_Intermediate/M1_Functions/L1_Intro.sql
@@ -0,0 +1,33 @@
+/*
+Functions:
+			These functions are predefined and used to perform user required operations.
+
+
+CATEGORES:	
+			1) Group OR Aggregate Functions
+			2) Scalar OR Single Row Functions
+
+
+1) Group or Aggregate Functions		:
+			
+			It perform a calculation on multiple rows and return a single result.
+
+			Ex: sum(), avg()
+
+
+2) Scalar OR Single Row Functions	:
+
+			It perform a calculation on each individual row and return one value per row
+
+
+			Ex:	lower(), length(), trim()
+
+
+
+TYPES OF FUNCTIONS:	
+			
+			1) String functions
+			2) Numeric functions
+			3) Date functions
+			4) Conversion functions
+*/
