d9c14a6d38b96b96bd5453673b37470957a47290
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-03-01 19:11:07 +0530

    Update Q6.sql

--- M9_Practical/M3_GroupBy/M1_Beginner/M5_General/Q6.sql
+++ M9_Practical/M3_GroupBy/M1_Beginner/M5_General/Q6.sql
@@ -1,10 +1,6 @@
 /*
 
- How would you identify daily active users (logged in at least once)?
-
-Identifying Daily Active Users (DAU) ?
-(or)
-Remove duplicate same-day logins first
+Identifying Daily Active Users (DAU) and Remove duplicate same-day logins first
 
 */
 
