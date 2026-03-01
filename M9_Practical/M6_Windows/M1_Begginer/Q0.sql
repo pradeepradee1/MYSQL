c439e4ad3f59ca508a63048229d13b4dcb04dee4
Pradeep <pradeepradee1@gmail.com>
2026-03-01 16:56:09 +0530

    changes added

---
+++ M9_Practical/M6_Windows/M1_Begginer/Q0.sql
@@ -0,0 +1,26 @@
+create or replace table Temp  (Id int, Email varchar(255));
+
+insert into Temp (Id, Email) 
+values 
+('1', 'john@example.com')
+('2', 'bob@example.com')
+('3', 'john@example.com');
+
+select * from Temp 
+
+/*
+Q:	Delete duplicate email entries based on its largest id ?
+
+*/
+
+
+
+DELETE FROM Temp
+WHERE Id IN (
+    SELECT Id FROM (
+        SELECT Id,
+               ROW_NUMBER() OVER (PARTITION BY Email ORDER BY Id DESC) AS rn
+        FROM Temp
+    ) t
+    WHERE rn > 1
+);
