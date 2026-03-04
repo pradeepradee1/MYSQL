6bf7f17a1586335fb09dac52ab13f1e4b9bef113
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-03-04 17:45:31 +0530

    Update Q1.sql

--- M9_Practical/M4_Join/M3_Amateur/Q1.sql
+++ M9_Practical/M4_Join/M3_Amateur/Q1.sql
@@ -1,11 +1,29 @@
 /*
 Question : Find the correspnding manager name
 
+
+CREATE TABLE Employees (
+    employee_id INT,
+    name VARCHAR(50),
+    manager_id INT
+);
+
+
+INSERT INTO Employees (employee_id, name, manager_id) 
+VALUES
+(1, 'Alice', 2),
+(2, 'Bob', 3),
+(3, 'Charlie', NULL);
+
+
+
 IP :
 
 	1	Alice		2
 	2	Bob			3
 	3	Charlie		
+
+
 
 
 OP 	:
