c73a24a584dfe38f02d6dfb4030f3116294b9205
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-03-04 16:08:06 +0530

    Update Q4.sql

--- M9_Practical/M4_Join/M2_Novice/Q4.sql
+++ M9_Practical/M4_Join/M2_Novice/Q4.sql
@@ -1,11 +1,13 @@
 /*
 
-Find Employees Whose Manager Left the Company 
-
-From an Employees table, find employees who:
+find employees who:
 Earn less than $30,000
 Report to a manager who has left the company
 When a manager leaves, their record is deleted — but their employee_id may still be referenced in manager_id.
+
+Find
+1) Earn less than 30000
+2) Report to a manager who no longer exists in the table
 
 */
 
@@ -50,28 +52,25 @@ Excepected Output :
 
 */
 
-/*
-Solution 1: Self LEFT JOIN
-*/
-
-SELECT 
-    e.employee_id
-FROM 
-    Employees e
-LEFT JOIN Employees m 
-    ON e.manager_id = m.employee_id
-WHERE e.salary < 30000 AND m.employee_id IS NULL AND e.manager_id IS NOT NULL
-ORDER BY e.employee_id;
 
 
 
 /* 
-Solution 2: Subquery with NOT IN 
+Solution 1: Subquery 
+
+
+We need :
+salary < 30000
+manager_id IS NOT NULL
+That manager_id does NOT exist in employee_id
+
 */
 
-SELECT employee_id
-FROM Employees
-WHERE salary < 30000 AND manager_id NOT IN 
- (SELECT DISTINCT employee_id FROM Employees)
-ORDER BY employee_id;
-
+SELECT 
+    e.*
+FROM 
+    Employees2 e
+WHERE e.salary < 30000
+    AND e.manager_id IS NOT NULL
+    AND NOT EXISTS (SELECT 1 FROM Employees2 m WHERE m.employee_id = e.manager_id
+  );
