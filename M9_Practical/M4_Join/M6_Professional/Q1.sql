8cccb8279d28d07eb66dd719675bb1937e6add7e
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-03-05 21:31:46 +0530

    Update Q1.sql

--- M9_Practical/M4_Join/M6_Professional/Q1.sql
+++ M9_Practical/M4_Join/M6_Professional/Q1.sql
@@ -1,12 +1,7 @@
 /*
 
-You are given a products table where a new row is inserted every time the price of a product changes. 
-Additionally, there is a transaction table containing details such as order_date and product_id for each order.
+calculating the total sales value of each product based on the price valid at the time of the order
 
-
-Write an SQL query to calculate the total sales value for each product, considering the cost of the product at the time 
-of the order date, display the output in ascending order of the product_id.
- 
 */
 
 /*
