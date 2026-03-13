6a53b237c9c6ad19140e719bcd86b33eab56db6c
Pradeep <pradeepradee1@gmail.com>
2026-03-13 13:12:33 +0530

    changes added

--- M4_Intermediate/M2_Group/L2_GroupClause.sql
+++ M4_Intermediate/M2_Group/L2_GroupClause.sql
@@ -6,43 +6,62 @@ GroupBy Clause:
 
 
 
-#findout max sal under each deptno?
-select eid,dno,max(sal) from emp
+/*
+findout max sal under each deptno?
+*/
+
+select 
+	eid,dno,max(sal) 
+from emp
 where dno is not null
 group by dno
 
 
 
-#find out number of emps working under each dept ?
-select dno, count(*) " No. of emps"
+/*
+find out number of emps working under each dept ?
+*/
+
+select 
+	dno, count(*) as " No. of emps"
 from emp
 where dno is not null
 GROUP BY dno;
 
 
-#find out number of emps working under each dept on order of deptno?
-select dno, count(*) " No. of emps"
+/*
+find out number of emps working under each dept on order of deptno?
+*/
+
+select 
+	dno, count(*) " No. of emps"
 from emp
 where dno is not null
 GROUP BY dno
 order by dno;
 
 
-#Group Concat
+/*
+Group Concat
+*/
+
 select 
-dno,GROUP_CONCAT(ename)
+	dno,
+	GROUP_CONCAT(ename)
 from emp
 where dno is not null
 GROUP BY dno;
 
 select 
-dno,GROUP_CONCAT(distinct ename order by ename desc)
+	dno,
+	GROUP_CONCAT(distinct ename order by ename desc)
 from emp
 where dno is not null
 GROUP BY dno;
 
 select 
-dno,GROUP_CONCAT(distinct ename order by ename desc separator '<->')
+	dno,
+	GROUP_CONCAT(distinct ename order by ename desc separator '<->')
 from emp
 where dno is not null
 GROUP BY dno;
--- M4_Intermediate/M2_Group/L3_HavingClause.sql
+++ M4_Intermediate/M2_Group/L3_HavingClause.sql
@@ -3,10 +3,13 @@
 				It is used to specify conditions on group by output.
 				
 
+
+find out number of emps working under each dept on order of deptno if a dept
+contains at least 10 emps?
+
+
 */
 
-# find out number of emps working under each dept on order of deptno if a dept
-# contains at least 10 emps?
 
 
 select eid ,ename ,dno, count(*) " No. of emps"
