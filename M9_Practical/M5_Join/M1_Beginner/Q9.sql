f9b13362c09527b39a9bbc51a54fc3898d458936
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-03-16 09:53:12 +0530

    Update Q9.sql

--- M9_Practical/M5_Join/M1_Beginner/Q9.sql
+++ M9_Practical/M5_Join/M1_Beginner/Q9.sql
@@ -33,6 +33,35 @@ INSERT INTO ps_orders (order_id, customer_id, amount, status) VALUES
 (104,3,660.0,'Delivered'),
 (105,3,300.0,'Delivered');
 
+
+Input :
+
+| customer_id | customer_name | customer_email                        |
+| ----------- | ------------- | ------------------------------------- |
+| 1           | Shashank      | [abc@gmail.com](mailto:abc@gmail.com) |
+| 2           | Rahul         | [aaa@gmail.com](mailto:aaa@gmail.com) |
+| 3           | Ajay          | [klm@gmail.com](mailto:klm@gmail.com) |
+| 4           | Nitin         | [poc@gmail.com](mailto:poc@gmail.com) |
+| 5           | Naveen        | [mnc@gmail.com](mailto:mnc@gmail.com) |
+
+
+| order_id | customer_id | amount | status    |
+| -------- | ----------- | ------ | --------- |
+| 101      | 1           | 550    | Delivered |
+| 102      | 2           | 350    | Delivered |
+| 103      | 1           | 220    | Cancelled |
+| 104      | 3           | 660    | Delivered |
+| 105      | 3           | 300    | Delivered |
+| 106      | 4           | 200    | Cancelled |
+| 107      | 4           | 150    | Cancelled |
+
+Output :
+
+| customer_id | customer_name |
+| ----------- | ------------- |
+| 4           | Nitin         |
+
+
 */
 
 
@@ -41,10 +70,11 @@ SELECT * from ps_customers
 
 SELECT * from ps_orders
 
-
-SELECT a.*
-FROM ps_customers a
-JOIN ps_orders b 
-ON a.customer_id = b.customer_id
-GROUP BY a.customer_id, a.customer_name, a.customer_email
-HAVING SUM(CASE WHEN b.status = 'Delivered' THEN 1 ELSE 0 END) = 0;
+SELECT 
+c.customer_id,
+c.customer_name
+FROM 
+    ps_customers c 
+JOIN ps_orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name
+HAVING COUNT(*) = SUM(CASE WHEN status = 'Cancelled' THEN 1 ELSE 0 END);
