972bdbb03d8e1bdd3f62c578f588718ec9bb2229
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-03-19 09:38:07 +0530

    Update Q2.sql

--- M9_Practical/M7_Subquery/M5_Senior/Q2.sql
+++ M9_Practical/M7_Subquery/M5_Senior/Q2.sql
@@ -36,6 +36,20 @@ VALUES (1), (2), (3), (5), (7), (8), (10);
 
 
 
+with recursive all_customer as (
+	  select min(customer_id) as minimum, max(customer_id) as maximum from Customers
+	  union all
+	  select minimum + 1,maximum from all_customer where minimum + 1 <= maximum
+)
+select 
+	a.minimum
+from 
+	all_customer a left join Customers b on a.minimum = b.customer_id 
+where b.customer_id is null
+
+ (or)
+
+
 WITH RECURSIVE All_IDs AS (
     SELECT 1 AS customer_id
     UNION ALL
