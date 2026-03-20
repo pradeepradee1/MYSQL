b92d9a5cfaa102c81b93cc59d49b084d3b47a795
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-03-20 08:14:16 +0530

    Update Q1.sql

--- M9_Practical/M7_Subquery/M6_Professional/Q1.sql
+++ M9_Practical/M7_Subquery/M6_Professional/Q1.sql
@@ -22,23 +22,42 @@ INSERT INTO Numbers (num) VALUES
 (10);
 
 
-WITH bounds AS (
-    SELECT MIN(num) AS min_val, MAX(num) AS max_val
-    FROM numbers
+
+WITH digits AS (
+    SELECT 0 d UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4
+    UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
 ),
-seq AS (
-    SELECT min_val + n AS num
-    FROM bounds
-    CROSS JOIN (
-        SELECT 0 n UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 
-        UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 
-        UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
-    ) t
-    WHERE min_val + n <= max_val
+bounds AS (
+    SELECT MIN(num) AS min_val, MAX(num) AS max_val FROM Numbers
+)
+SELECT a.*
+FROM digits a
+CROSS JOIN bounds b
+LEFT JOIN Numbers n 
+    ON a.d = n.num
+WHERE n.num IS NULL
+  AND a.d BETWEEN b.min_val AND b.max_val;
+
+
+
+(OR)
+
+WITH digits AS (
+    SELECT 0 d UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4
+    UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
+),
+numbers AS (
+    -- Generate numbers from 1 to 100
+    SELECT (t.d * 10 + u.d) + 1 AS num
+    FROM digits t
+    CROSS JOIN digits u
 )
 
-SELECT s.num AS missing_number
-FROM seq s
-LEFT JOIN numbers n
-ON s.num = n.num
-WHERE n.num IS NULL;
+SELECT n.num AS missing_number
+FROM numbers n
+LEFT JOIN Numbers t
+    ON n.num = t.num
+WHERE n.num BETWEEN (SELECT MIN(num) FROM Numbers)
+                AND (SELECT MAX(num) FROM Numbers)
+  AND t.num IS NULL
+ORDER BY n.num;
