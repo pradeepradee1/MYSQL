f3a8ad18849ca94dfd059f20ae35e0fe17046bf2
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-05-25 07:23:41 +0530

    Update Q1.sql

--- M9_Practical/M4_GroupBy/M1_Beginner/M6_General_with_date/Q1.sql
+++ M9_Practical/M4_GroupBy/M1_Beginner/M6_General_with_date/Q1.sql
@@ -1,10 +1,25 @@
 /*
 
-You are given a table of customer orders with the following columns: 
-order_id, customer_id, order_date, and order_amount. 
-
 Write a query to calculate the total order amount for each customer in the year 2020, 
 including customers with no orders in 2020.
+
+
+| order_id | customer_id | order_date | order_amount |
+| -------- | ----------- | ---------- | ------------ |
+| 1        | 123         | 2020-02-15 | 100.00       |
+| 2        | 456         | 2020-05-20 | 200.00       |
+| 3        | 789         | 2021-01-10 | 300.00       |
+| 4        | 123         | 2019-12-31 | 150.00       |
+| 5        | 456         | 2020-11-05 | 50.00        |
+| 6        | 789         | 2020-09-15 | 75.00        |
+
+
+| customer_id | total_order_amount |
+| ----------- | ------------------ |
+| 123         | 100.00             |
+| 456         | 250.00             |
+| 789         | 75.00              |
+
 
 */
 
