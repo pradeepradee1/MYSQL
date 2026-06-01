b3630cb263a3fed0894bd1c2d210099b12481d35
Pradeep <pradeepradee1@gmail.com>
2026-06-01 11:48:21 +0530

    Renamed the value

--- M6_Professional/M1_SubQuery/M2_TypesOf_Subquery/M1_TypesOf_SubQuery.sql
+++ M6_Professional/M1_SubQuery/M2_TypesOf_Subquery/M1_TypesOf_SubQuery.sql
@@ -1,9 +1,9 @@
 /*
 Types :
 		
-		1)	Single Row Subquery
-		2)	Multiple Row Subquery
-		3) 	Multiple-Column Subquery
+		1)	Single value Subquery
+		2)	Multiple value Subquery
+		3) 	Multiple_Column and Multiple_value Subquery
 		4) 	Correlated Subquery
 		5)	Subquery in SELECT Clause
 		6) 	Subquery in FROM Clause
