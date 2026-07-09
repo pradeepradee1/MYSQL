5d486ae3feb6c0b9095516c0686588dafb12c3dd
Pradeep <pradeepradee1@gmail.com>
2026-07-09 11:49:48 +0530

    changea are added

---
+++ M4_Intermediate/M2_Scalar/L2_Advance_String.sql
@@ -0,0 +1,78 @@
+/*
+#Character classes:
+
+[abc] 			=	Either a or b or c
+[^abc] 			=	Except a and b and c
+[a-z] 			=	Lower alpha
+[A-Z] 			=	Upper alpha
+[0-9] 			=	Numeric
+[a-zA-Z0-9] 	=	alphanumeric
+[^a-zA-Z0-9] 	=	Except alphanumeric(Special Characters)
+
+
+#Pre defined Character classes:
+#Note : Pre defined class is not working
+
+\s 				=	Space Character
+\S 				=	Except Space Character
+\w 				=	Alphanumeric [a-zA-Z0-9]
+\W 				=	except Alphanumeric (Special Characters)
+\d 				=	Numerical
+\D 				=	Except Numerical
+.  				=	Any character including special characters
+
+#Quantifier
+
+
+[]*				=	occurs 0 or moretime (based on bodo MAS )
+[]+				=	occurs 1 or moretimes
+[]?				=	occurs 0 or 1 time 		
+[]{n}			=	occurs n time
+[]{n,}			=	occurs n or more time
+[]{y,z}			=	occurs atleast y time but less than z time
+^x 				= 	It will check whether target string starts with x or not
+x$ 				= 	It will check whether target string ends with x or not
+
+
+
+#Example
+
+1) Mobile Number start with 8 or 9 and total digit = 10
+		
+		[89][0-9]{9}
+
+2)First Character upper case , contains lower case alaphbets, only one digit allowed in between
+		
+		[A-Z][a-z]*[0-9][a-z]*
+
+3) Email ID (Pradeepradee1@gmail.com)
+		
+		[a-zA-Z0-9_\-\.]+[@][a-z]+[\.][a-z]{2,3}
+		
+
+
+*/
+
+#1) REGEXP_REPLACE
+SELECT REGEXP_REPLACE('abc123','[0-9]','');
+/*
+    OP
+    abc
+*/
+
+
+#2) REGEXP_SUBSTR
+SELECT REGEXP_SUBSTR('abc123xyz','[0-9]+');
+/*
+    OP
+    123
+*/
+
+
+# This is like operator in special operator
+#3) REGEXP_LIKE
+SELECT REGEXP_LIKE('user@example.com','^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
+/*
+    OP
+    1
+*/
