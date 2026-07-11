541b75cc78b0ce458d03bff0251f76aa2dab0369
Pradeep <pradeepradee1@gmail.com>
2026-07-11 10:21:37 +0530

    changes are added

---
+++ M9_Practical/M1_Basic/M0_Notes.sql
@@ -0,0 +1,12 @@
+/*
+Note : 
+DB Revenue and Profit and Net Income ?
+
+Revenue :
+Revenue is the total income a business generates from sales or services, also known as the "top line". 
+
+Profit or Net Income : 
+Profit, or net income  is the money left over after all expenses (such as costs of goods, operating expenses, and taxes) 
+are subtracted from the revenue
+
+*/
