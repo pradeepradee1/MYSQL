78fbda3e0d5d51ea2a6f204e2d133d94c5f4a195
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-07-12 10:28:52 +0530

    Update Q4.sql

--- M9_Practical/M4_GroupBy/M1_Beginner/M3_Duplicate/Q4.sql
+++ M9_Practical/M4_GroupBy/M1_Beginner/M3_Duplicate/Q4.sql
@@ -1,6 +1,8 @@
 /*
 
-Identifying Daily Active Users (DAU) and Remove duplicate same-day logins first
+1) Remove duplicate logins by the same user on the same day
+2) Count the number of unique users who logged in each day (Daily Active Users - DAU)
+
 
 */
 
@@ -26,6 +28,36 @@ INSERT INTO user_activity1 (user_id, login_date) VALUES
 (5, '2025-10-13'),
 (2, '2025-10-13'),
 (1, '2025-10-13');
+
+Sample Input :
+
+| user_id | login_date |                                         |
+| ------: | :--------- | --------------------------------------- |
+|       1 | 2025-10-10 |                                         |
+|       2 | 2025-10-10 |                                         |
+|       1 | 2025-10-11 |                                         |
+|       3 | 2025-10-11 |                                         |
+|       2 | 2025-10-11 |                                         |
+|       1 | 2025-10-12 |                                         |
+|       4 | 2025-10-12 |                                         |
+|       3 | 2025-10-12 |                                         |
+|       1 | 2025-10-12 | ← Duplicate login (same user, same day) |
+|       5 | 2025-10-13 |                                         |
+|       2 | 2025-10-13 |                                         |
+|       1 | 2025-10-13 |                                         |
+
+Sample Output :
+
+| login_date | daily_active_users |
+| :--------- | -----------------: |
+| 2025-10-10 |                  2 |
+| 2025-10-11 |                  3 |
+| 2025-10-12 |                  3 |
+| 2025-10-13 |                  3 |
+
+
+
+
 
 */
 
