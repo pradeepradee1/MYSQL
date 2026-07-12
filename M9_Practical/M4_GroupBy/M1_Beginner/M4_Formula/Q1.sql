b6b05a04c27837021c3b1639d4f7342c04bbfe5e
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-07-12 11:05:37 +0530

    Update Q1.sql

--- M9_Practical/M4_GroupBy/M1_Beginner/M4_Formula/Q1.sql
+++ M9_Practical/M4_GroupBy/M1_Beginner/M4_Formula/Q1.sql
@@ -50,6 +50,23 @@ Volume of Rectangular 	:   V = l × w × h
 
 
 /*
+| name     | product_id | units |
+| -------- | ---------: | ----: |
+| LCHouse1 |          1 |     1 |
+| LCHouse1 |          2 |    10 |
+| LCHouse1 |          3 |     5 |
+| LCHouse2 |          1 |     2 |
+| LCHouse2 |          2 |     2 |
+| LCHouse3 |          4 |     1 |
+
+| product_id | product_name | width | length | height |
+| ---------: | ------------ | ----: | -----: | -----: |
+|          1 | LC-TV        |     5 |     50 |     40 |
+|          2 | LC-KeyChain  |     5 |      5 |      5 |
+|          3 | LC-Phone     |     2 |     10 |     10 |
+|          4 | LC-T-Shirt   |     4 |     10 |     20 |
+
+
 Output:		
 		 Name        Volume	
 		LCHouse1	 12250
