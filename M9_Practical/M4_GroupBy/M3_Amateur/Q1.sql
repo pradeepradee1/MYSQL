7235815ab127c97b13f7984934c1136efeb206f1
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-07-14 09:31:27 +0530

    Update Q1.sql

--- M9_Practical/M4_GroupBy/M3_Amateur/Q1.sql
+++ M9_Practical/M4_GroupBy/M3_Amateur/Q1.sql
@@ -4,13 +4,9 @@ Questins :
 
 Identify Active Users Based on Video Uploads and Interactions
 
-based on the following criteria:
-
-Users must have uploaded at least 5 videos.
-At least 3 of these videos must have more than 100 interactions (likes + comments).
-
-You need to return a list of user IDs and the count of their "highly interactive" videos, 
-sorted by the count in descending order.
+*) The user has uploaded at least 5 videos.
+*) At least 3 of those videos have more than 100 total interactions
+    Total Interactions = Likes + Comments
 
 */
 --------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -41,6 +37,30 @@ INSERT INTO videos (video_id, user_id, upload_date, likes, comments) VALUES
 (14, 3, '2025-01-03', 80, 20),
 (15, 3, '2025-01-04', 150, 40),
 (16, 3, '2025-01-05', 70, 10);
+
+
+
+Input :
+
+| video_id | user_id | upload_date | likes | comments |
+| -------: | ------: | :---------: | ----: | -------: |
+|        1 |       1 |  2025-01-01 |    50 |       20 |
+|        2 |       1 |  2025-01-02 |   120 |       30 |
+|        3 |       1 |  2025-01-03 |   200 |       50 |
+|        4 |       1 |  2025-01-04 |   150 |       60 |
+|        5 |       1 |  2025-01-05 |    80 |       10 |
+|        6 |       1 |  2025-01-06 |   130 |       40 |
+|        7 |       2 |  2025-01-01 |    60 |       20 |
+|        8 |       2 |  2025-01-02 |    50 |       10 |
+|        9 |       2 |  2025-01-03 |   150 |       30 |
+|       10 |       2 |  2025-01-04 |    90 |       15 |
+|       11 |       2 |  2025-01-05 |   200 |       40 |
+|       12 |       3 |  2025-01-01 |   120 |       50 |
+|       13 |       3 |  2025-01-02 |   130 |       60 |
+|       14 |       3 |  2025-01-03 |    80 |       20 |
+|       15 |       3 |  2025-01-04 |   150 |       40 |
+|       16 |       3 |  2025-01-05 |    70 |       10 |
+
 
 
 Output : 
