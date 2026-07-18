e5ea8349eb9efd5c55b767973f11979ca960ad7a
Pradeep <54977250+pradeepradee1@users.noreply.github.com>
2026-07-18 19:03:44 +0530

    Update M1_TypesOf_SubQuery.sql

--- M6_Professional/M1_SubQuery/M2_TypesOf_Subquery/M1_TypesOf_SubQuery.sql
+++ M6_Professional/M1_SubQuery/M2_TypesOf_Subquery/M1_TypesOf_SubQuery.sql
@@ -1,8 +1,8 @@
 /*
 Types :
 		
-		1)	Single value Subquery
-		2)	Multiple value Subquery
+		1)	Single_Column and Single value Subquery
+		2)	Single_Column and Multiple value Subquery
 		3) 	Multiple_Column and Multiple_value Subquery
 		4) 	Correlated Subquery
 		5)	Subquery in SELECT Clause
